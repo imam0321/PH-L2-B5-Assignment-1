e1926e535cc2e33098bc6d71a8bf84788d364967
imam0321 <imam.hossain0321@gmail.com>
2025-05-07 23:05:51 +0600

    solve problem 1

--- index.ts
+++ index.ts
@@ -1,29 +1,10 @@
+// Problem 1:
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const formatString = (input: string, toUpper?: boolean): string => {
+  if (input && toUpper === true) {
+    return input.toUpperCase();
+  } else if (input && toUpper === false) {
+    return input.toLowerCase();
+  }
+};
+console.log(formatString("imam", false));
